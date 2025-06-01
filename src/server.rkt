3287b0cf49466ad550583faf4cbe7705400596f4
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-06-01 14:22:04 +0200

    feat(tcp-server): add logging
    Use Racket's logging capabilities to create a logger and log-receiver.
    Create a separate thread that gets log messages and print them out.
    Logging thread runs under the same custodian as the server thread.

--- src/server.rkt
+++ src/server.rkt
@@ -2,11 +2,30 @@
 
 ;;; =============================== Imports ====================================
 
-(require racket/tcp)
+(require racket/tcp
+         racket/date)
 
 ;;; =============================== Exports ====================================
 
 (provide serve)
+
+;;; =============================== Logging ====================================
+
+(define lg (make-logger))
+(define nrc (make-log-receiver lg 'info))
+
+(define exit-log-loop-channel (make-channel))
+
+(define (log-loop)
+  (sync
+   (handle-evt nrc
+                 (λ (v)
+                   (printf "[~a] ~a: ~a\n"
+                           (date->string (current-date) #t)
+                           (vector-ref v 0)
+                           (vector-ref v 1))
+                   (log-loop)))
+   exit-log-loop-channel))
 
 ;;; =========================== Request handling ===============================
 
@@ -19,17 +38,28 @@
 
 (define (accept-and-handle listener)
   (define-values (in out) (tcp-accept listener))
+
+  (log-info "Accepted")
+
   (handle in out)
   (close-input-port in)
   (close-output-port out))
 
 (define (serve [port-no 11211])
   (define cust (make-custodian))
-  (parameterize ([current-custodian cust])
+  (parameterize ([current-custodian cust]
+                 [current-logger lg]
+                 [date-display-format 'iso-8601])
     (define listener (tcp-listen port-no 5 #t))
     (define (loop)
       (accept-and-handle listener)
       (loop))
-    (thread loop))
-  (λ ()
-    (custodian-shutdown-all cust)))
+    (define log-thread (thread log-loop))
+    (thread loop)
+    (log-info "Server started")
+    (with-handlers ([exn:break? (lambda (e)
+                                  (log-info "Server shutdown")
+                                  (channel-put exit-log-loop-channel #t)
+                                  (thread-wait log-thread)
+                                  (custodian-shutdown-all cust))])
+      (sync/enable-break never-evt))))
