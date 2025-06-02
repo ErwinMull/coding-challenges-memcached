f007f6eed577fc3f3793becb322737779763a93e
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-06-02 20:47:58 +0200

    feat: parse command line to struct
    Also features a rename of the struct 'protocol' to 'command'.

--- src/protocol.rkt
+++ src/protocol.rkt
@@ -1,11 +1,44 @@
 #lang racket/base
 
-(struct protocol
-  (command-name key flags exptime byte-count noreply data-block)
-  #:exra-constructor-name make-protocol
-  #:transparent)
+;;; =============================== Imports ====================================
+
+(require racket/list)
+
+;;; ============================== Constants ===================================
+
+(define VALID-COMMANDS (list 'set 'get))
+
+;;; =============================== Structs ====================================
+
+[struct command
+  (name key flags exptime byte-count noreply)
+  #:extra-constructor-name make-command
+  #:transparent]
 
 (struct storage-unit
   (flags bytecount data-block)
   #:extra-constructor-name make-storage-unit
   #:transparent)
+
+(define (bytes->symbol b)
+  (string->symbol (bytes->string/utf-8 b)))
+
+(define (bytes->number b)
+  (string->number (bytes->string/utf-8 b)))
+
+(define (bytes->command b)
+  (define parts (regexp-split #rx" " b))
+  (define len (length parts))
+  (when (not (or (= len 5) (= len 6)))
+    (error "TODO: custom exception? 1"))
+  (define name (bytes->symbol (first parts)))
+  (when (not (member name VALID-COMMANDS))
+    (error "TODO: custom exception? 2"))
+  (define key (bytes->symbol (second parts)))
+  (define flags (bytes->number (third parts)))
+  (define exptime #f)
+  (define byte-count (bytes->number (fifth parts)))
+  (define noreply (and (= len 6)
+                       (or (bytes=? (sixth parts) #"noreply")
+                           (error "TODO: custom exception? 3"))))
+  (make-command name key flags exptime byte-count noreply))
