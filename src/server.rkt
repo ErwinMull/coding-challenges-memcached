beec25fa70838f2d88414ea03cb4b06653c9b219
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-06-04 21:04:24 +0200

    refactor: server.rkt
    Changes:
    - add optional timeout parameter and byte-reading procedures that are
      able to timeout
    - adjust to protocol.rkt API
    - adjust handle procedures
    - change some log messages

--- src/server.rkt
+++ src/server.rkt
@@ -4,12 +4,27 @@
 
 (require racket/tcp
          racket/date
+         racket/port
 
+         "key-val-store.rkt"
          "protocol.rkt")
 
 ;;; =============================== Exports ====================================
 
 (provide serve/test)
+
+;;; ============================== Constants ===================================
+
+(define timeout-secs (make-parameter #f))
+
+;;; ============================== Exceptions ==================================
+
+(struct exn:timeout exn ()
+  #:extra-constructor-name make-exn:timeout
+  #:transparent)
+
+(define (raise-timeout message)
+  (raise (make-exn:timeout message (current-continuation-marks))))
 
 ;;; =============================== Logging ====================================
 
@@ -43,34 +58,77 @@
                 (and (number? try-read) (= try-read 0)))
       (loop))))
 
+(define (make-timeout-alarm secs)
+  (alarm-evt (+ (current-inexact-milliseconds) (* secs 1000))))
+
+(define (sync/timeout-alarm event)
+  (sync event
+        (if (timeout-secs)
+            (wrap-evt (make-timeout-alarm (timeout-secs))
+                      (λ (v) (raise-timeout "IDLE_TIMEOUT")))
+            never-evt)))
+
+(define (read-bytes-line/timeout [in (current-input-port)]
+                                 [mode 'return-linefeed])
+  (sync/timeout-alarm (handle-evt (read-bytes-line-evt in mode)
+                                  (λ (v) v))))
+
+(define (read-bytes/timeout amt [in (current-input-port)])
+  (sync/timeout-alarm (handle-evt (read-bytes-evt amt in)
+                                  (λ (v) v))))
+
+(define (write-bytes/flush b out)
+  (write-bytes b out)
+  (flush-output out))
+
 (define (handle in out)
-  (with-handlers ([exn:fail? (λ (exn)
-                               (write-bytes #"ERROR" out))])
-    (define b (read-bytes-line in 'return-linefeed))
-    (define com (bytes->command b))
-    (define data
-      (and (storage-command? com)
-           (read-bytes (command-byte-count com) in)))
-    (drain-input-port in)
-    (log-info (format "Received: ~a ~a ~a"
-                      (command-name com)
-                      (command-key com)
-                      data))
-    (write-bytes #"RECEIVED" out)))
+  (let loop ()
+    (with-handlers ([exn:timeout?
+                     (λ (e)
+                       (log-info "Timeout")
+                       (write-bytes/flush (bytes-append
+                                           (string->bytes/utf-8
+                                            (exn-message e))
+                                           #"\r\n")
+                                          out))]
+                    [exn:fail:network?
+                     (λ (e)
+                       (void))]
+                    [exn:command-parse?
+                     (λ (e)
+                       (drain-input-port in)
+                       (log-error "COMMAND PARSE")
+                       (loop))])
+      (define first-line (read-bytes-line/timeout in))
+      (unless (eof-object? first-line)
+        (define com (bytes->command first-line))
+        (define data (and (storage-command? com)
+                          (read-bytes/timeout (storage-command-bytecount com)
+                                              in)))
+
+        (log-info (format "Received: ~a ~a"
+                          (command-name com)
+                          (command-key com)))
+        (define result
+          (case (command-name com)
+            [(set) (handle-set com data)]
+            [(get) (handle-get com)]))
+
+        (write-bytes/flush result out)
+        (drain-input-port in)
+        (loop)))))
 
 ;;; ========================= Server & connections =============================
 
 (define (accept-and-handle listener)
-  (define cust (make-custodian))
-  (parameterize ([current-custodian cust])
-    (define-values (in out) (tcp-accept listener))
-    (thread (λ ()
-              (handle in out)
-              (close-input-port in)
-              (close-output-port out))))
-  (void (thread (λ ()
-                  (sleep 10)
-                  (custodian-shutdown-all cust)))))
+  (define-values (in out) (tcp-accept listener))
+  (log-info "Connection opened")
+  (void
+   (thread (λ ()
+             (handle in out)
+             (close-input-port in)
+             (close-output-port out)
+             (log-info "Connection terminated")))))
 
 (define (serve/private port-no
                        #:test? [test? #f]
@@ -84,7 +142,8 @@
     (void (thread (λ ()
                     (let loop ()
                       (accept-and-handle listener)
-                      (loop)))))
+                      (loop))))
+          (init-key-val-thread))
 
     (cond
       [test?
