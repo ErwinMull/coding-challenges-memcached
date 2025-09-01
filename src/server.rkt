83ecd5a7cac4f950c660a1afd05d3c2deede5d37
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-09-01 18:10:34 +0200

    refactor: add connection hash table & remove code
    1. Introduce a hash table that holds every input and output port. These
       are consolidated in a synchronizable event for when data is sent by
       the client.
    2. Remove old code of handling requests.
    3. Adjust log thread loop to enable breaks.

--- src/server.rkt
+++ src/server.rkt
@@ -41,27 +41,72 @@
   (thread
    (thunk
     (let loop ()
-      (sync (handle-evt
-             nrc
-             (λ (vec)
-               (define level (vector-ref vec 0))
-               (define message (vector-ref vec 1))
-               (print-log-message port
-                                  (date->string (current-date))
-                                  level
-                                  message)
-               (loop))))))))
+      (sync/enable-break (handle-evt
+                          nrc
+                          (λ (vec)
+                            (define level (vector-ref vec 0))
+                            (define message (vector-ref vec 1))
+                            (print-log-message port
+                                               (date->string (current-date))
+                                               level
+                                               message)
+                            (loop))))))))
 
-;;; =========================== Request handling ===============================
+;;; ============================= CONNECTIONS ==================================
 
-(define (handle in out)
-  (let* ([buf (make-bytes (expt 2 16) 0)])
-    (define nread (read-bytes-avail! buf in))
-    (define message (subbytes buf 0 nread))
-    (log-info (format "Received ~a bytes: ~v"
-                      nread
-                      message))
-    (write-bytes message out)))
+(define (tcp-connection-evt in out eof-return-value)
+  (choice-evt (handle-evt (eof-evt in)
+                          (λ _
+                            eof-return-value))
+              (handle-evt (read-bytes-line-evt in 'return-linefeed)
+                          (λ (line)
+                            (void
+                             (when (not (eof-object? line))
+                               (enqueue b)))))))
+
+(struct connection-table (ht [evt #:mutable])
+  #:property prop:evt (λ (conn-tab)
+                        (handle-evt
+                         (connection-table-evt conn-tab)
+                         (λ (res)
+                           (when (and (not (void? res))
+                                      (hash-has-key? table res))
+                             (connection-pool-remove! conn-pool res))))))
+
+(define (initialize-connection-table)
+  (connection-table (make-hasheq) (choice-evt)))
+
+(define (connection-table-generate-event! conn-tab)
+  (set-connection-table-evt! conn-tab
+                             (handle-evt
+                              (apply choice-evt
+                                     (map (λ (item)
+                                            (define-values (id in out)
+                                              (apply values item))
+                                            (tcp-connection-evt in out id))))
+                              (λ (res)
+                                (when (and (not (void? res))
+                                           (hash-has-key? table res))
+                                  (connection-tab-remove! conn-tab res))))))
+
+(define (connection-table-set! conn-tab in out)
+  (define id (gensym))
+  (define lst `(,in ,out))
+  (hash-set! (connection-table-ht conn-pool) id lst)
+  (connection-table-generate-event! conn-tab))
+
+(define (connection-table-remove! conn-tab id)
+  (define ht (connection-table-ht conn-tab))
+  (define ports (hash-ref ht id))
+  (hash-remove! ht id)
+  (for-each tcp-abandon-port ports)
+  (connection-table-generate-event! conn-tab))
+
+(define (connection-pool-clear! conn-pool)
+  (define table (connection-pool-table conn-pool))
+  (for-each (curry for-each tcp-abandon-port) (hash-values table))
+  (hash-clear! table)
+  (set-connection-table-evt! (choice-evt)))
 
 ;;; ========================= Server & connections =============================
 
