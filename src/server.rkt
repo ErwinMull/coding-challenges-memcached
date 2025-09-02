38f9c6a0e782c664401f0cdbfedf513de0ac4baf
ErwinMull <122201580+ErwinMull@users.noreply.github.com>
2025-09-02 19:26:57 +0200

    refactor: refactor serve and some minor bugs

--- src/server.rkt
+++ src/server.rkt
@@ -5,7 +5,8 @@
 (require racket/tcp
          racket/function
          racket/vector
-         racket/date)
+         racket/date
+         racket/port)
 
 ;;; ================================ CONFIG ====================================
 
@@ -13,14 +14,6 @@
 (define WORKER-THREADS-AMOUNT 16)
 
 ;;; ============================ WORKER THREADS ================================
-
-(define QUEUE (make-channel))
-
-(define (enqueue val)
-  (channel-put QUEUE val))
-
-(define (dequeue)
-  (channel-get QUEUE))
 
 (define (make-worker-thread-pool n thnk)
   (for/list ([_ (in-range n)])
@@ -37,20 +30,37 @@
 (define (print-log-message port date-str level message)
   (fprintf port "[~a] ~a: ~a\n" date-str level message))
 
-(define (setup-log-thread nrc port)
-  (thread
-   (thunk
-    (let loop ()
-      (sync/enable-break (handle-evt
-                          nrc
-                          (λ (vec)
-                            (define level (vector-ref vec 0))
-                            (define message (vector-ref vec 1))
-                            (print-log-message port
-                                               (date->string (current-date))
-                                               level
-                                               message)
-                            (loop))))))))
+(define (setup-log-thread [nrc (make-log-receiver (current-logger) LOG-LEVEL)]
+                          [port (current-output-port)])
+  (define t
+    (thread
+     (thunk
+      (let loop ()
+        (sync/enable-break (handle-evt
+                            nrc
+                            (λ (vec)
+                              (define level (vector-ref vec 0))
+                              (define message (vector-ref vec 1))
+                              (print-log-message port
+                                                 (date->string (current-date))
+                                                 level
+                                                 message)
+                              (loop)))
+                           (thread-receive-evt))))))
+  (thunk
+   (thread-send t 1 (thunk (void)))
+   (thread-wait t)))
+
+;;; ============================ REQUEST QUEUE =================================
+
+(define QUEUE (make-channel))
+
+(define (enqueue/line&out line out)
+  (channel-put QUEUE (vector line out)))
+
+(define (dequeue/line&out)
+  (define tmp (channel-get QUEUE))
+  (values (vector-ref tmp 0) (vector-ref tmp 1)))
 
 ;;; ============================= CONNECTIONS ==================================
 
@@ -62,37 +72,34 @@
                           (λ (line)
                             (void
                              (when (not (eof-object? line))
-                               (enqueue b)))))))
+                               (enqueue/line&out line out)))))))
 
 (struct connection-table (ht [evt #:mutable])
   #:property prop:evt (λ (conn-tab)
-                        (handle-evt
-                         (connection-table-evt conn-tab)
-                         (λ (res)
-                           (when (and (not (void? res))
-                                      (hash-has-key? table res))
-                             (connection-pool-remove! conn-pool res))))))
+                        (connection-table-evt conn-tab)))
 
 (define (initialize-connection-table)
   (connection-table (make-hasheq) (choice-evt)))
 
 (define (connection-table-generate-event! conn-tab)
+  (define table (connection-table-ht conn-tab))
   (set-connection-table-evt! conn-tab
                              (handle-evt
                               (apply choice-evt
                                      (map (λ (item)
                                             (define-values (id in out)
                                               (apply values item))
-                                            (tcp-connection-evt in out id))))
+                                            (tcp-connection-evt in out id))
+                                          (hash->list table)))
                               (λ (res)
                                 (when (and (not (void? res))
                                            (hash-has-key? table res))
-                                  (connection-tab-remove! conn-tab res))))))
+                                  (connection-table-remove! conn-tab res))))))
 
 (define (connection-table-set! conn-tab in out)
   (define id (gensym))
   (define lst `(,in ,out))
-  (hash-set! (connection-table-ht conn-pool) id lst)
+  (hash-set! (connection-table-ht conn-tab) id lst)
   (connection-table-generate-event! conn-tab))
 
 (define (connection-table-remove! conn-tab id)
@@ -102,67 +109,59 @@
   (for-each tcp-abandon-port ports)
   (connection-table-generate-event! conn-tab))
 
-(define (connection-pool-clear! conn-pool)
-  (define table (connection-pool-table conn-pool))
+(define (connection-table-clear! conn-tab)
+  (define table (connection-table-ht conn-tab))
   (for-each (curry for-each tcp-abandon-port) (hash-values table))
   (hash-clear! table)
-  (set-connection-table-evt! (choice-evt)))
+  (set-connection-table-evt! conn-tab (choice-evt)))
 
-;;; ========================= Server & connections =============================
+;;; ================================ SERVER ====================================
 
-(define (accept-and-handle listener)
-  (define cust (make-custodian))
-  (parameterize ([current-custodian cust])
-    (define-values (in out) (tcp-accept listener))
-    (thread (λ ()
-              (handle in out)
-              (close-input-port in)
-              (close-output-port out))))
-  (void (thread (λ ()
-                  (sleep 10)
-                  (custodian-shutdown-all cust)))))
-
-(define (serve/private port-no
-                       #:test? [test? #f]
-                       #:log-file [log-file (current-output-port)])
+(define (serve [port-no 11211]
+               #:log? [log? #f]
+               #:log-file [log-file (current-output-port)])
   (define cust (make-custodian))
   (parameterize ([current-custodian cust]
-                 [current-logger lg]
+                 [current-logger (make-logger)]
+                 [current-output-port log-file]
                  [date-display-format 'iso-8601])
+    (define kill-log-thread
+      (if log?
+          (setup-log-thread)
+          (thunk (void))))
+    (log-info (format "Starting server on port ~a" port-no))
     (define listener (tcp-listen port-no 5 #t))
+    (define conn-tab (initialize-connection-table))
+    (define thread-pool
+      (make-worker-thread-pool WORKER-THREADS-AMOUNT
+                               (thunk
+                                (define-values (line out) (dequeue/line&out))
+                                (log-info (format "Received: ~a" line))
+                                (write-bytes (bytes-append #"ECHO: "
+                                                           line
+                                                           #"\r\n")
+                                             out)
+                                (flush-output out))))
+    (with-handlers ([exn:break? (λ (e)
+                                  (log-info "Shutting down server")
+                                  (connection-table-clear! conn-tab)
+                                  (kill-worker-thread-pool thread-pool)
+                                  (tcp-close listener)
+                                  (kill-log-thread)
+                                  (custodian-shutdown-all cust))])
+      (let loop ()
+        (sync/enable-break
+         (handle-evt (tcp-accept-evt listener)
+                     (λ (in-out-lst)
+                       (connection-table-set! conn-tab
+                                              (car in-out-lst)
+                                              (cadr in-out-lst))
+                       (loop)))
+         (handle-evt (connection-table-evt conn-tab)
+                     (λ _
+                       (loop))))))))
 
-    (void (thread (λ ()
-                    (let loop ()
-                      (accept-and-handle listener)
-                      (loop)))))
-
-    (cond
-      [test?
-       (λ () (custodian-shutdown-all cust))]
-      [else
-       (define shutdown-log-thread
-         (let* ([chan (make-channel)]
-                [log-loop (log-setup chan log-file)]
-                [t (thread log-loop)])
-           (λ ()
-             (channel-put chan #t)
-             (thread-wait t))))
-       (log-info "Server started")
-       (with-handlers ([exn:break? (lambda (e)
-                                     (log-info "Server shutdown")
-                                     (shutdown-log-thread)
-                                     (custodian-shutdown-all cust))])
-         (sync/enable-break never-evt))])))
-
-(define (serve/test [port-no 11211])
-  (define stop (serve/private port-no
-                              #:test? #t))
-  stop)
-
-(define (serve [port-no 11211])
-  (serve/private port-no))
-
-;;; ================================= Main =====================================
+;;; ================================= MAIN =====================================
 
 (module+ main
-  (serve))
+  (serve #:log? #t))
